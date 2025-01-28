99e6acbd2f750ee8b0300bb56fcbe3e5e4940244
Ayush Jain <ayushshreeshreemal@gmail.com>
2025-01-28 12:09:48 -0500

    recipes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,12 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
-import List from './qcomps/list_keys_id'
+import RecipeList from './qcomps/recipes'
 
 export default function Home() {
   return (
     <div>
-      <List />
+      <RecipeList />
     </div>
     
   )
