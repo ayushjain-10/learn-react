4f67adfcec33edea71a059f9b89798105ae247bc
Ayush Jain <ayushshreeshreemal@gmail.com>
2025-02-03 13:03:03 -0500

    react-states

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
-import StuckForm from './qcomps/stuckForm'
-
+import RequestTracker from './qcomps/shoppingCart'
+import Scoreboard from './qcomps/updObjectsForm'
+import BucketList from './qcomps/arrObj'
+import Gallery from './qcomps/state'
 export default function Home() {
   return (
     <div>
-      <StuckForm />
+      <Gallery />
     </div>
     
   )
